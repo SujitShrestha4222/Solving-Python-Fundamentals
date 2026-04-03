5136d7b195687c9d5e3c64d242055aa8cf7a4be2
Sujit Shrestha <sujitxtha4222@gmail.com>
2026-04-03 15:40:32 +0545

    Solved qns related to updating list values based on condition i.e Double even values in list

---
+++ Easy_Task/Answers/Task_1.py
@@ -0,0 +1,27 @@
+lst = [1, 2, 3, 4, 5]
+new_lst = []
+
+for i in lst:
+    if i % 2 == 0:
+        new_lst. append (i**2)
+    else:
+        new_lst. append (i)
+
+print (new_lst)
+
+
+
+
+
+"""-----OR-----"""
+
+
+
+
+
+lst1 = [12, 13, 14, 15]
+
+for i in range (len(lst1)):   # means range is from 0 to 4 (basically becomes like index of lst)
+    if lst1[i] % 2 == 0:
+        lst1[i] = lst1[i]**2    #or lst[i] = lst[i] * lst[i]
+print (lst1)
