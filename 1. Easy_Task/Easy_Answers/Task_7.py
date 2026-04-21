a83ef9acf6d2ca9d9198c78631bbe8819437a130
Sujit Shrestha <sujitxtha4222@gmail.com>
2026-04-21 22:56:59 +0545

    updated Task-7(Easy) ans

--- 1. Easy_Task/Easy_Answers/Task_7.py
+++ 1. Easy_Task/Easy_Answers/Task_7.py
@@ -5,3 +5,19 @@ for num in nums:
         break
 else:
      print ("Loop completed!!!")
+
+"""
+Output: No output
+Because:
+- The else block we wrote is outside of for loop(not inside of for loop).
+- The codition to run else block of a for loop is ---> when the loop get executed normally (i.e.without break).
+However,
+- Here, the loop stops early when num == 3, so the else block is skipped.
+"""
+# Example when else block is exectued and we get output: Loop completed!!!
+# nums = [1, 2, 3, 4, 5]
+# for num in nums:
+#     if num == 9:
+#         break
+# else:
+#      print ("Loop completed!!!")
